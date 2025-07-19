272459f8c5c0ff045f527146f26c3edc6bb09c8f
Rodrigo807 <148443665+Rodrigo807@users.noreply.github.com>
2025-07-19 13:14:43 +0100

    ascii art project

---
+++ 1-introduction-to-kotlin/Art.kt
@@ -0,0 +1,25 @@
+package org.example
+
+fun main() {
+    /*
+         .....
+      .         .
+     .   0  0    .
+      ...........
+       .. __ ..
+      . .|C_|. .
+     .  ......  .
+         .  .
+         .  .
+    */
+    println("         .....")
+    println("      .         .")
+    println("     .   0  0    .")
+    println("      ...........")
+    println("       .. __ ..")
+    println("      . .|C_|. .")
+    println("     .  ......  .")
+    println("         .  .")
+    println("         .  .")
+
+}
